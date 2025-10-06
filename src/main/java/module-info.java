b3f9fe74227666036eb7cc35893a65691c723568
curtishd <1023632660@qq.com>
2025-10-06 15:57:51 +0800

    fix: Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module me.cdh {
+    requires java.desktop;
+    requires kotlin.stdlib;
+    requires kotlinx.coroutines.swing;
+    requires kotlinx.coroutines.core;
+    requires com.miglayout.swing;
+    requires com.miglayout.core;
+    requires com.formdev.flatlaf;
+    requires me.cdh.palette;
+}
